299a3ed6cb5ec5c7d02faaebc191d213d1c5b160
Mateusz Rojek <mateuszr2047@gmail.com>
2024-04-03 11:51:30 +0200

    Utworzenie projektu

---
+++ src/test/java/pl/mateusz/example/bettingapp/BettingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mateusz.example.bettingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BettingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
